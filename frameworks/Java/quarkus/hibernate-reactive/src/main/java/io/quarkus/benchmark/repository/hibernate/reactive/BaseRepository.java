52f1a9838062825367583d0ad77b38417cdd0491
Stephane Epardaud <stef@epardaud.fr>
2020-06-11 12:14:42 +0200

    HR version

---
+++ frameworks/Java/quarkus/hibernate-reactive/src/main/java/io/quarkus/benchmark/repository/hibernate/reactive/BaseRepository.java
@@ -0,0 +1,22 @@
+package io.quarkus.benchmark.repository.hibernate.reactive;
+
+import java.util.function.Function;
+
+import javax.inject.Inject;
+
+import org.hibernate.reactive.mutiny.Mutiny;
+
+import io.smallrye.mutiny.Uni;
+
+public class BaseRepository {
+    @Inject
+    protected Mutiny.SessionFactory sf;
+
+    public <T> Uni<T> inSession(Function<Mutiny.Session, Uni<T>> work){
+        return sf.openSession().flatMap(session -> {
+            return work.apply(session)
+                    .onItemOrFailure().invoke((w, t) -> session.close());
+        });
+    }
+
+}
